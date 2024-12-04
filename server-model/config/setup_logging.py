0e51553396098aae17817bd90f983537f0b065da
rahfianugerah <nrahfia@gmail.com>
2024-12-04 12:31:06 +0700

    chore: add code documentation on setup log function

--- server-model/config/setup_logging.py
+++ server-model/config/setup_logging.py
@@ -1,16 +1,31 @@
+# Import required library
 import logging
 
 # Function to configure logging
 def log():
+    """
+    Configures logging for the application.
+
+    This function sets up logging to both a file and the console with the following properties:
+    - Logs messages at the INFO level or higher.
+    - The log messages include the timestamp, logger name, log level, and message content.
+    - Logs are saved in a file named "app.log" in the application's directory.
+    - Logs are also displayed on the console (stdout).
+
+    Returns:
+        logger (logging.Logger): Configured logger instance for the application.
+    """
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         handlers=[
-            # Log to a file
+            # Log to a file named 'app.log'
             logging.FileHandler("app.log"),
-            # Log to console
+            # Log to the console (stdout)
             logging.StreamHandler()
         ]
     )
+
+    # Create and return a logger instance with the name 'EzTrip ML-Model Server'
     logger = logging.getLogger("EzTrip ML-Model Server")
     return logger
